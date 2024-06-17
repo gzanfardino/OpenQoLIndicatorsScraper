c639e665fab26f77c2176746718418741b812e7c
Gennaro Zanfardino <stud.gennaro.zanfardino@gmail.com>
2024-06-17 16:02:29 +0200

    trying out new update to webdriver

--- ARTA_scraper.py
+++ ARTA_scraper.py
@@ -1,49 +1,39 @@
 # Description: This script scrapes the data from the website and stores it in a csv file
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 
 from datetime import timedelta, date, datetime
 import pandas as pd
 import time
 
 options = Options()
-options.headless = False
 options.add_argument("--window-size=1920,1200")
+options.add_argument("--headless")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=Service(GeckoDriverManager().install()), options=options)
+
 driver.implicitly_wait(10)
 driver.get("https://sira.artaabruzzo.it/#/stazioni-fisse") 
 
 CURRENT_DATE = date.today()
-DAYS_TO_SCRAPE = 3000
+DAYS_TO_SCRAPE = 500
 
 partial_tables = []
 try:
     for i in range(1, DAYS_TO_SCRAPE):
         last_fetched_date = ""
         #Making a copy of the current data.csv to avoid changing it
-        with open('data.csv', 'r') as f:
-            current_line = ""
-            
-            # Suffixing it with the current datetime to avoid overwriting the previous copy 
-            with open(f'data_{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.csv', 'w') as f1:
-                for line in f:
-                    f1.write(line)
-                    current_line = line
-
-            # Storing the last line of the in a date variable
-            last_line =  datetime.strptime(current_line.split(',')[-1], '%Y-%m-%d').date()
-
 
         #Waiting for the table to load and storing it in a variable
-        html_table = driver.find_element(By.XPATH, '//*[@id="cdk-accordion-child-1"]/div/table')
-
+        #html_table = driver.find_element(By.XPATH, '//*[@id="cdk-accordion-child-1"]/div/table')
+        html_table = driver.find_element(By.XPATH, '/html/body/app-root/main-nav/mat-sidenav-container/mat-sidenav-content/div[2]/app-stazioni-fisse/div[2]/div[2]/mat-expansion-panel[2]/div/div/table')
         #Waiting for half a second to let the table load and avoid getting blocked
         time.sleep(0.5)
 
@@ -56,7 +46,7 @@ try:
 
         #Converting the table to a pandas dataframe
         pd_table = pd.read_html(html_table.get_attribute('outerHTML'))
-
+        print(f"Scraping data for the date: {CURRENT_DATE - timedelta(days=i - 1)}")
         #Computing the date of the selected table
         selected_date = CURRENT_DATE - timedelta(days=i - 1)
         pd_table[0]['Date'] = selected_date
@@ -76,7 +66,7 @@ except:
 full_table = pd.concat(partial_tables)
 
 #Storing the table in a csv file
-full_table.to_csv(f'data.csv', index=False)
+full_table.to_csv(f'datau.csv', index=False)
 
 driver.save_screenshot('screenshot.png')
 driver.quit()
